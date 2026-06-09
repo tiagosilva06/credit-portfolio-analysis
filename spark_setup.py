342d661b77c62e2105088e3bf7d3e620c13cfbd1
Tiago Santos <tiagosilva9278@gmail.com>
2026-06-09 15:37:01 -0300

    chore: initial project structure and environment setup

---
+++ spark_setup.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ['PYSPARK_PYTHON'] = r'C:\Tiago\data engineer\Projetos\credit-portfolio-analysis\venv\Scripts\python.exe'
+os.environ['PYSPARK_DRIVER_PYTHON'] = r'C:\Tiago\data engineer\Projetos\credit-portfolio-analysis\venv\Scripts\python.exe'
+os.environ['JAVA_HOME'] = r'C:\Program Files\Eclipse Adoptium\jdk-17.0.14.7-hotspot'
